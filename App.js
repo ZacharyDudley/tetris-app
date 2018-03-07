1f56e206911b118c20119e7f71b0344cd49199a8
Zachary Dudley Friedman <ZacharyDFriedman@gmail.com>
2018-03-07 08:29:22 -0600

    first

---
+++ App.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react'
+import { StyleSheet, Text, View, Button } from 'react-native'
+import { StackNavigator } from 'react-navigation'
+import { Board } from './Board'
+
+
+class Menu extends Component {
+  render() {
+    return(
+      <View style={ styles.container }>
+        <Text>TETRIS</Text>
+        <Button
+          title='play'
+          onPress={() => {
+            this.props.navigation.navigate('Game')}}
+        />
+      </View>
+    )
+  }
+}
+
+class Game extends Component {
+  render() {
+    return(
+      <View style={ styles.container }>
+        <Board />
+      </View>
+    )
+  }
+}
+
+const RootStack = StackNavigator({
+  Menu: {
+    screen: Menu
+  },
+  Game: {
+    screen: Game
+  }
+}, {
+  initialRouteName: 'Menu'
+})
+
+export default class App extends Component {
+  render() {
+    return <RootStack />
+  }
+}
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+})
